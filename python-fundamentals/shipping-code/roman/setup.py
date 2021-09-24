334246a6677e3b76a3b05f25c729cad30a6454e3
Iain Mitchell <iainjmitchell@gmail.com>
2021-09-24 13:48:27 +0100

    adding new course content

--- python-fundamentals/shipping-code/roman/setup.py
+++ python-fundamentals/shipping-code/roman/setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="roman",
     version="1.0",
-    py_modules=["roman"],   
+    py_modules=["roman"],
     author="Iain",
     author_email="iainjmitchell@gmail.com",
     description="Roman numeral solution",
